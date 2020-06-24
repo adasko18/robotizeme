c1dc75d0357a8fe9f43479975e34b0cfece49db2
Adam Waniczek <adam.waniczek@gmail.com>
2020-06-24 22:09:42 +0200

    add Maven Configuration

---
+++ src/main/java/io/github/adasko18/robotizemeapi/RobotizemeApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.adasko18.robotizemeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotizemeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotizemeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/adasko18/robotizemeapi/RobotizemeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adasko18.robotizemeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotizemeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
